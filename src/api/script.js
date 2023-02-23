dedf957bfe1f0d5a1e2319ab7cfc1bc86db65ce1
YuraStarchenko <100568416+YuraStarchenko@users.noreply.github.com>
2023-02-23 12:50:50 +0200

    +

--- src/api/script.js
+++ src/api/script.js
@@ -1,10 +1,12 @@
-
-
+import Notiflix from "notiflix";
+import simpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { fetchData } from "./api";
 import { createMarkupImg } from "./markup"
 
-
-const form = document.getElementById("search-form");
+const refs = {
+  form: document.getElementById('search-form'),
+};
 
 form.addEventListener('submit', onSubmitForm);
 

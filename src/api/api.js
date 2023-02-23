723c51c6c360406d83c46e2d136204b9a985a672
YuraStarchenko <100568416+YuraStarchenko@users.noreply.github.com>
2023-02-23 20:58:59 +0200

    +

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ const BASE_URL = 'https://pixabay.com/api';
 const KEY = '33854415-dab75466e51d96ca7439b60b4';
 const searchOptions = 'image_type=photo&orientation=horizontal&safesearch=true';
 
-export function fetchPhotos(query, page) {
-  const search = `${BASE_URL}?key=${KEY}&q=${query}&${searchOptions}&page=${page}$per_page=30`;
-	return axios.get(URL);
+export function fetchPhotos(searchQuery, page) {
+  const search = `${BASE_URL}?key=${KEY}&q=${searchQuery}&${searchOptions}&page=${page}$per_page=30`;
+  return axios.get(search);
 }
--- src/api/script.js
+++ src/api/script.js
@@ -8,16 +8,23 @@ const refs = {
   form: document.getElementById('search-form'),
 };
 
-form.addEventListener('submit', onSubmitForm);
+refs.form.addEventListener('submit', onSubmitForm);
 
 
 function onSubmitForm(e) {
 	e.preventDefault();
+	
+	const searchQuery = e.currentTarget.elements.searchQuery.value;
+	
+	const options = {
+    headers: {
+      Authorization: '33854415-dab75466e51d96ca7439b60b4',
+    },
+	};
+	
+	const url = `https://pixabay.com/api/images/?q=${searchQuery}&image_type=photo`;
 
-	const form = e.currentTarget;
-	const value = form.elements.searchQuery.value.trim();
-
-	fetchData(value).then(({ hits }) => {
-		console.log(hits);
- });
+	fetch(url, options)
+		.then(r => r.json())
+		.then(console.log);
 }

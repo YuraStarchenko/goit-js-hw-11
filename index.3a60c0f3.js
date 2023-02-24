df6da827fafb70fc60fe056b0e34e2a499bfe352
YuraStarchenko <100568416+YuraStarchenko@users.noreply.github.com>
2023-02-24 09:17:06 +0000

    Deploying to gh-pages from @ YuraStarchenko/goit-js-hw-11@8e7849c21332d7f9964e6153021074d013ec0025 🚀

---
+++ index.3a60c0f3.js
@@ -0,0 +1,2 @@
+({form:document.getElementById("search-form")}).form.addEventListener("submit",(function(e){e.preventDefault();const t=e.currentTarget.elements.searchQuery.value.trim();console.log(t)}));
+//# sourceMappingURL=index.3a60c0f3.js.map
---
+++ index.8c031a51.js
@@ -0,0 +1,2 @@
+document.getElementById("search-form").addEventListener("submit",(function(e){e.preventDefault();var t=e.currentTarget.elements.searchQuery.value.trim();console.log(t)}));
+//# sourceMappingURL=index.8c031a51.js.map

929ee19ee1ad088e1e070499c06807f550757ad4
YuraStarchenko <100568416+YuraStarchenko@users.noreply.github.com>
2023-02-24 10:19:17 +0200

    +

--- src/api/script.js
+++ src/api/script.js
@@ -1,30 +1,14 @@
-import Notiflix from "notiflix";
-import simpleLightbox from "simplelightbox";
-import 'simplelightbox/dist/simple-lightbox.min.css';
-import { fetchData } from "./api";
-import { createMarkupImg } from "./markup"
+// import Notiflix from 'notiflix';
+// import simpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+// import { fetchData } from './api';
+// import { createMarkupImg } from './markup';
 
-const refs = {
-  form: document.getElementById('search-form'),
-};
+// const refs = {
+//   form: document.getElementById('search-form'),
+// };
 
-refs.form.addEventListener('submit', onSubmitForm);
+// refs.form.addEventListener('submit', onSubmitForm);
 
+// function onSubmitForm() {}
 
-function onSubmitForm(e) {
-	e.preventDefault();
-	
-	const searchQuery = e.currentTarget.elements.searchQuery.value;
-	
-	const options = {
-    headers: {
-      Authorization: '33854415-dab75466e51d96ca7439b60b4',
-    },
-	};
-	
-	const url = `https://pixabay.com/api/images/?q=${searchQuery}&image_type=photo`;
-
-	fetch(url, options)
-		.then(r => r.json())
-		.then(console.log);
-}

3780c8c326d1322ff341330d23af02d8305912b4
YuraStarchenko <100568416+YuraStarchenko@users.noreply.github.com>
2023-02-26 12:36:55 +0200

    +

--- src/service/script.js
+++ src/service/script.js
@@ -1,41 +1,33 @@
-// import Notiflix from 'notiflix';
-// import simpleLightbox from 'simplelightbox';
-// import 'simplelightbox/dist/simple-lightbox.min.css';
-// import PixabayApiService from './pixabayAPI.js';
-// import  createMarkup from './markup';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { refs } from './refs';
 import createMarkup from './markup';
 import PixabayApiService from './pixabayAPI.js';
-
-const refs = {
-  searchForm: document.getElementById('search-form'),
-  gallery: document.querySelector('.gallery'),
-  loadMoreBtn: document.querySelector('.load-more'),
-};
-
-// loadMoreBtn.classList.add('is-hidden');
 
 const pixabayApiService = new PixabayApiService();
 
 refs.searchForm.addEventListener('submit', onSearch);
 refs.loadMoreBtn.addEventListener('click', onLoadMore);
 
-function onSearch(e) {
+async function onSearch(e) {
   e.preventDefault();
   clearGalleryContainer();
   pixabayApiService.query = e.currentTarget.searchQuery.value.trim();
+
   if (pixabayApiService.query === '') {
     return Notify.failure(
       'Sorry, there are no images matching your search query. Please try again.'
     );
-  }
+	}
+
   pixabayApiService.resetPage();
-  pixabayApiService.fetchHits().then(appendGalleryMarkup);
+	pixabayApiService.fetchHits().then(appendGalleryMarkup);
 }
 
 function onLoadMore() {
-  pixabayApiService.fetchHits().then(appendGalleryMarkup);
+	pixabayApiService.fetchHits().then(appendGalleryMarkup);
 }
 
 function appendGalleryMarkup(hits) {

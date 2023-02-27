0fcefae458f21929f58e11a8e78784f4580e04f8
YuraStarchenko <100568416+YuraStarchenko@users.noreply.github.com>
2023-02-27 13:52:20 +0200

    +

--- src/service/LoadMoreBtn.js
+++ src/service/LoadMoreBtn.js
@@ -26,3 +26,42 @@ export default class LoadMoreBtn {
     this.button.textContent = 'Load more';
   }
 }
+
+// function addTotalInfoCounter(data) {
+//   Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+// }
+
+// function noInfoForSearch() {
+//   Notiflix.Notify.failure('Please specify your search query.');
+// }
+
+// function endOfContent() {
+//   Notiflix.Notify.warning(
+//     "We're sorry, but you've reached the end of search results."
+//   );
+// }
+
+// function alertNoContentFound() {
+//   Notiflix.Notify.failure(
+//     'Sorry, there are no images matching your search query. Please try again.'
+//   );
+// }
+
+// let totalHits = 0;
+
+  // if (query === '') {
+  //   noInfoForSearch();
+  //   return;
+  // }
+
+	    //   if (data.totalHits === 0) {
+      //   alertNoContentFound();
+      // } else {
+      //   createCards(data.hits);
+      //   const lightbox = new SimpleLightbox('.gallery a', {
+      //     captionDelay: 250,
+      //   }).refresh();
+      //   addTotalInfoCounter(data);
+
+      //   if (data.totalHits > perPage) {
+      //     loadMoreButton.classList.remove('is-hidden');
